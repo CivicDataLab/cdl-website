346e16f35848ce485b7d4b3f05d1b91c849d187a
sanjaypinna <sanjaypinna265@gmail.com>
2024-06-04 12:38:24 +0530

    add bold text

--- app/collaborate/page.tsx
+++ app/collaborate/page.tsx
@@ -11,8 +11,8 @@ const collaborate = () => {
             <div className=" lg:max-w-md">
               <h2 className="text-2xl font-bold mb-3">Work with us!</h2>
               <h2 className="text-xl ">
-                We champion collaboration as a catalyst for meaningful change, and uphold our commitment to utilising data for public good, embracing
-                diverse perspectives and a multi-disciplinary approach to inspire creative problem-solving.
+                We champion collaboration as a catalyst for meaningful change, and uphold our commitment to utilising <b>data for public good,</b>{" "}
+                embracing diverse perspectives and a multi-disciplinary approach to inspire creative problem-solving.
               </h2>
             </div>
           </div>
@@ -23,8 +23,13 @@ const collaborate = () => {
           <div className="flex  justify-center flex-wrap items-center py-16 px-8 lg:p-40 gap-10 lg:gap-32 flex-grow">
             <div className="lg:max-w-md">
               <h2 className="text-2xl font-bold mb-3"> Careers</h2>
-              <h2 className="text-xl mb-3">For all hiring related queries, please write to us at careers@civicdatalab.in.</h2>
-              <h2 className="text-xl "> Check out our openings page to know more about the roles we`re hiring for. </h2>
+              <h2 className="text-xl mb-3">
+                For all hiring related queries, please write to us at <b>careers@civicdatalab.in.</b>
+              </h2>
+              <h2 className="text-xl ">
+                {" "}
+                Check out our <b>openings</b> page to know more about the roles we`re hiring for.{" "}
+              </h2>
             </div>
             <Image src="/collab-sec2.png" width={380} height={260} className="object-contain" alt="civicdatalab logo" />
           </div>
@@ -36,10 +41,15 @@ const collaborate = () => {
             <Image src="/collab-sec3.png" width={350} height={200} className="object-contain" alt="civicdatalab logo" />
             <div className=" lg:max-w-md">
               <h2 className="text-2xl font-bold mb-3">Collaborate</h2>
-              <h2 className="text-xl mb-3">
-                Join us in co-creating service, product and research solutions to make meaningful impact. Write to us at info@civicdatalab.in.
+              <h2 className="text-xl mb-3">Join us in co-creating service, product and research solutions to make meaningful impact.</h2>
+              <h2 className="text-xl  mb-3">
+                {" "}
+                Write to us at <b>info@civicdatalab.in.</b>
               </h2>
-              <h2 className="text-xl "> We&apos;re on Twitter, LinkedIn, Instagram and Github. Reach out to us there as well! </h2>
+              <h2 className="text-xl ">
+                {" "}
+                We&apos;re on <b> Twitter, LinkedIn, Instagram</b> and <b>Github.</b> Reach out to us there as well!{" "}
+              </h2>
             </div>
           </div>
         </div>
