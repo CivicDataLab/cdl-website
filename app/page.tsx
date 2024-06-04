b3eb3e275a39a539e3bf33adc9afc612d3a7c26c
pixeledcode <ashoaib540918@gmail.com>
2024-06-04 15:42:07 +0530

    add carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 'use client'
 
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from '@/components/Carousel/Carousel'
 import { Globe } from '@/components/Globe'
 import Image from 'next/image'
 import { ReactTyped } from 'react-typed'
@@ -211,6 +218,70 @@ export default function Home() {
 							Know More &gt;&gt;
 						</a>
 					</div>
+				</div>
+			</section>
+
+			{/* Partners */}
+			<section className="container py-16">
+				<Heading>Our Partners</Heading>
+
+				<Carousel
+					opts={{
+						loop: true,
+						align: 'start',
+					}}
+					className="w-full"
+				>
+					<CarouselContent className="-ml-1">
+						{Array.from({ length: 10 }).map((_, index) => (
+							<CarouselItem
+								key={index}
+								className="pl-1 md:basis-1/2 lg:basis-1/5"
+							>
+								<div className="p-1">
+									<div className="flex aspect-square items-center justify-center p-6">
+										<span className="text-2xl font-semibold">{index + 1}</span>
+									</div>
+								</div>
+							</CarouselItem>
+						))}
+					</CarouselContent>
+					<CarouselPrevious />
+					<CarouselNext />
+				</Carousel>
+			</section>
+
+			{/* Team */}
+			<section className="bg-light-blue">
+				<div className="container py-16">
+					<Heading>Meet Our Bandhus</Heading>
+
+					<Carousel
+						opts={{
+							loop: true,
+							align: 'start',
+						}}
+						className="w-full"
+					>
+						<CarouselContent className="-ml-1">
+							{Array.from({ length: 10 }).map((_, index) => (
+								<CarouselItem
+									key={index}
+									className="pl-1 md:basis-1/2 lg:basis-1/5"
+								>
+									<div className="p-1">
+										<div className="flex aspect-square items-center justify-center p-6">
+											<span className="text-2xl font-semibold">
+												{index + 1}
+											</span>
+										</div>
+									</div>
+								</CarouselItem>
+							))}
+						</CarouselContent>
+						<CarouselPrevious />
+						<CarouselNext />
+					</Carousel>
 				</div>
 			</section>
 		</main>
