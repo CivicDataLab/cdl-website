76f3c4c3c2dd7d94ce8cffe4d16da4401b485645
pixeledcode <ashoaib540918@gmail.com>
2024-06-07 17:23:32 +0530

    work page fix

--- app/work/client.tsx
+++ app/work/client.tsx
@@ -1,29 +1,38 @@
-"use client";
+'use client'
 
-import React, { Suspense, useEffect, useState } from "react";
-import Image from "next/image";
-import Details from "@/components/Details/Details";
+import Details from '@/components/Details/Details'
+import Image from 'next/image'
 
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/Carousel/Carousel";
-import { WorkCollection } from "@/types/work-collection";
-import { getStrapiData, getStrapiMediaUrl } from "@/lib/utils";
-import { WorkTypes } from "@/types/work";
-import Markdown from "react-markdown";
-const queries = ["work_img", "capacity_media", "details.image"];
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from '@/components/Carousel/Carousel'
+import { getStrapiData, getStrapiMediaUrl } from '@/lib/utils'
+import { WorkTypes } from '@/types/work'
+import { WorkCollection } from '@/types/work-collection'
+import Markdown from 'react-markdown'
+
+const queries = ['work_img', 'capacity_media', 'details.image']
 export async function WorkClient({
-  data,
+	data,
 }: {
-  data: {
-    [key: string]: WorkCollection["data"];
-  };
+	data: {
+		[key: string]: WorkCollection['data']
+	}
 }) {
-return (
+	const strapiData: WorkTypes = await getStrapiData(`/work`, queries)
+	const maindata = strapiData.data.attributes
+
+	return (
 		<div>
 			<section>
 				<div className="bg-light-green w-full">
 					<div className="flex  justify-center flex-wrap items-center py-16 px-8 lg:p-10 gap-10 lg:gap-26 flex-grow">
 						<Image
-							src="/work-sec1.png"
+							src={getStrapiMediaUrl(maindata.work_img.data.attributes.url)}
 							width={600}
 							height={390}
 							className="object-contain"
@@ -31,28 +40,20 @@ return (
 						/>
 						<div className=" lg:max-w-lg">
 							<h2 className=" font-heading lg:text-2xl sm:text-lg text-center font-medium mb-3">
-								We work to enhance
-								<br />
-								<b>data-driven decision-making</b> processes
-								<br /> and
-								<br />
-								enable <b>citizen-participation</b> <br />
-								in governance.
+								<Markdown>{maindata.work_desc}</Markdown>
 							</h2>
 						</div>
 					</div>
 				</div>
 			</section>
-			<Details data={data} />
+			<Details data={data} content={maindata.details} />
 			<section className=" bg-light-green">
 				<div className=" container w-full lg:p-16 p-10 ">
 					<div>
 						<h2 className="font-heading font-extrabold text-2xl mb-3">
-							Capacity building
+							{maindata.capacity_title}
 						</h2>
-						<p className="text-xl">
-							A short about Civicdatalab&apos;s capacity building efforts
-						</p>
+						<p className="text-xl">{maindata.capacity_desc}</p>
 					</div>
 					<Carousel
 						opts={{
@@ -62,16 +63,19 @@ return (
 						className="w-full"
 					>
 						<CarouselContent className="-ml-1">
-							{Array.from({ length: 10 }).map((_, index) => (
+							{maindata.capacity_media.data.map((item: any) => (
 								<CarouselItem
-									key={index}
+									key={item.id}
 									className="pl-1 md:basis-1/2 lg:basis-1/5"
 								>
 									<div className="p-1">
 										<div className="flex aspect-square items-center justify-center p-6">
-											<span className="text-2xl font-semibold">
-												{index + 1}
-											</span>
+											<Image
+												src={getStrapiMediaUrl(item.attributes.url)}
+												alt=""
+												width={150}
+												height={150}
+											/>
 										</div>
 									</div>
 								</CarouselItem>
