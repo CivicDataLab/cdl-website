f8c84ca0af884fd83d9e632d1c7857405eaeaf89
sanjaypinna <sanjaypinna265@gmail.com>
2024-06-08 17:23:15 +0530

    integrate strapi

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,141 +1,98 @@
-'use client'
+"use client";
 
-import React, { Suspense, useEffect, useState } from 'react'
-import Image from 'next/image'
-import Details from '@/components/Details/Details'
-import { parseAsString, useQueryState } from 'nuqs'
+import React, { Suspense, useState } from "react";
+import Image from "next/image";
+import { getStrapiData, getStrapiMediaUrl } from "@/lib/utils";
+import { ResourceTypes } from "@/types/resource";
+import Markdown from "react-markdown";
 
-export default function Resources() {
-	return (
-		<Suspense>
-			<Client />
-		</Suspense>
-	)
+interface ResourceProps {
+  strapiData: ResourceTypes;
 }
 
-function Client() {
-	const ResourcesData = [
-		{
-			name: 'Dashboard/Tools',
-			desc: 'We help made public data open, accessible and usable by designing platforms to host and analyze well-curated datasets. We do this as part of our mission to bolster digital public infrastructure (DPI) and aid data-driven governance, foster collaborative learning, enhance citizen engagement, and nurture the open data ecosystem.',
-			res: [
-				{
-					name: 'IDS-DRR',
-					img: '/resource-sec2.1.png',
-				},
-				{
-					name: 'Justice Hub',
-					img: '/resource-sec2.2.png',
-				},
-				{
-					name: 'Open Budgets India',
-					img: '/resource-sec2.3.png',
-				},
-				{
-					name: 'Open Contracting India',
-					img: '/resource-sec2.4.png',
-				},
-			],
-		},
-		{
-			name: 'Blogs',
-			desc: 'Read our latest blogs to stay informed about our work and insights in various fields.',
-			res: [
-				{
-					name: 'Justice Hub',
-					img: '/resource-sec2.2.png',
-				},
-			],
-		},
-		{
-			name: 'Reports',
-			desc: 'Access our comprehensive reports that provide detailed analysis and findings.',
-			res: [
-				{
-					name: 'IDS-DRR',
-					img: '/resource-sec2.1.png',
-				},
-			],
-		},
-		{
-			name: 'Guidebook',
-			desc: 'Explore our guidebooks that provide step-by-step instructions and valuable tips.',
-			res: [
-				{
-					name: 'Justice Hub',
-					img: '/resource-sec2.2.png',
-				},
-			],
-		},
-		{
-			name: 'Policy papers',
-			desc: 'Review our policy papers that delve into significant topics and recommendations.',
-			res: [
-				{
-					name: 'IDS-DRR',
-					img: '/resource-sec2.1.png',
-				},
-				{
-					name: 'Justice Hub',
-					img: '/resource-sec2.2.png',
-				},
-			],
-		},
-	]
+const ResourcePage: React.FC<ResourceProps> = ({ strapiData }) => {
+  const items = strapiData.data.attributes.description.split("\n").filter((item) => item.trim() !== ""); // Splitting data into individual items
 
-	const [filter, setFilter] = useQueryState(
-		'filter',
-		parseAsString.withDefault('Dashboard/Tools')
-	)
-	const [selected, setSelected] = useState(() => {
-		return (
-			ResourcesData.find((item) => item.name === filter) || ResourcesData[0]
-		)
-	})
+  const [selected, setSelected] = useState(strapiData.data.attributes.details[0]); // Default to the first item
 
-	useEffect(() => {
-		const found = ResourcesData.find((item) => item.name === filter)
-		if (found) {
-			setSelected(found)
-		}
-	}, [filter])
+  return (
+    <div>
+      <section>
+        <div className="bg-light-blue w-full">
+          <div className="flex justify-center flex-wrap items-center py-16 px-8 lg:p-16 gap-10 lg:gap-26 flex-grow">
+            <Image
+              src={getStrapiMediaUrl(strapiData.data.attributes.image.data.attributes.url)}
+              width={520}
+              height={320}
+              className="object-contain"
+              alt="book logo"
+              style={{
+                width: "520px",
+                height: "320px",
+              }}
+            />
+            <div className="lg:max-w-md">
+              <Markdown className={"text-dark-blue font-heading text-2xl font-bold mb-3"}>{strapiData.data.attributes.heading_title}</Markdown>
+              <ul className="list-disc ml-12 marker:text-dark-blue">
+                {items.map((item, index) => (
+                  <li key={index} className="text-xl">
+                    {item.startsWith("- ") ? item.slice(2) : item}
+                  </li>
+                ))}
+              </ul>{" "}
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="p-6 mb-32">
+        <div className="flex gap-4 p-2 justify-start overflow-auto scrollbar-hide lg:justify-center">
+          {strapiData.data.attributes.details.map((item, index) => (
+            <button
+              key={index}
+              onClick={() => setSelected(item)}
+              className={`whitespace-nowrap rounded-3xl border-2 w-fit h-fit px-3 py-1 ${
+                selected.title === item.title ? "bg-black text-white" : "border-black"
+              }`}
+            >
+              {item.title}
+            </button>
+          ))}
+        </div>
+        <div className="flex justify-center flex-wrap mt-8 gap-16 px-5">
+          <div className=" w-full 2xl:w-1/6 ">
+            <h2 className="font-heading text-2xl font-bold mb-3">{selected.title}</h2>
+            <p className="text-xl">{selected.description}</p>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+            {selected.resources?.map((details, idx) => (
+              <div key={idx} className=" block w-80">
+                <a href={details.link} target="_blank">
+                  <Image
+                    src={getStrapiMediaUrl(details.image.data.attributes.url)}
+                    width={300}
+                    height={300}
+                    className="object-contain"
+                    alt={details.title}
+                  />
+                  <p className="text-xl mt-4">{details.title}</p>
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-	useEffect(() => {
-		if (selected.name !== filter) {
-			setFilter(selected.name)
-		}
-	}, [selected])
+const Resource = async () => {
+  const queries = ["image", "details", "details.resources.image"];
+  const strapiData: ResourceTypes = await getStrapiData(`/resource`, queries);
 
-	return (
-		<div>
-			<section>
-				<div className="bg-light-blue w-full">
-					<div className="flex justify-center flex-wrap items-center py-16 px-8 lg:p-16 gap-10 lg:gap-26 flex-grow">
-						<Image
-							src="/resource-sec1.png"
-							width={520}
-							height={320}
-							className="object-contain"
-							alt="book logo"
-							style={{
-								width: '520px',
-								height: '320px',
-							}}
-						/>
-						<div className="lg:max-w-md">
-							<h2 className="text-dark-blue font-heading text-2xl font-bold mb-3">
-								Explore the resources
-								<br /> we provide to
-							</h2>
-							<ul className="list-disc ml-12 marker:text-dark-blue">
-								<li className="text-xl">support digital commons</li>
-								<li className="text-xl">share knowledge and tools</li>
-								<li className="text-xl">enhance awareness</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</section>
-		</div>
-	)
-}
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResourcePage strapiData={strapiData} />
+    </Suspense>
+  );
+};
+export default Resource;
---
+++ types/resource.ts
@@ -0,0 +1,70 @@
+export interface ResourceTypes {
+  data: Data;
+}
+
+interface Data {
+  id: number;
+  attributes: Attributes;
+}
+
+interface Attributes {
+  description: string;
+  heading_title: string;
+  details: Details[];
+  image: Image;
+}
+
+interface Details {
+  uuid: string;
+  title: string;
+  description: string;
+  image: Image;
+  resources: Resources[];
+}
+
+interface Resources {
+  uuid: string;
+  title: string;
+  link: string;
+  image: Image;
+}
+
+type ImageFormat = {
+  name: string;
+  hash: string;
+  ext: string;
+  mime: string;
+  path: null | string;
+  width: number;
+  height: number;
+  size: number;
+  sizeInBytes: number;
+  url: string;
+};
+
+type Image = {
+  data: {
+    id: number;
+    attributes: {
+      name: string;
+      alternativeText: null | string;
+      caption: null | string;
+      width: number;
+      height: number;
+      formats: {
+        thumbnail: ImageFormat;
+        small: ImageFormat;
+      };
+      hash: string;
+      ext: string;
+      mime: string;
+      size: number;
+      url: string;
+      previewUrl: null | string;
+      provider: string;
+      provider_metadata: null | string;
+      createdAt: string;
+      updatedAt: string;
+    };
+  };
+};
