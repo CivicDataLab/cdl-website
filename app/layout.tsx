defbc2d9b31e67f3477c1849789da401c36bbbea
sanjaypinna <sanjaypinna265@gmail.com>
2025-04-04 19:59:07 +0530

    restrict indexing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,46 @@
-import { Footer } from '@/components/Footer/Footer'
-import { Navbar } from '@/components/Navbar/Navbar'
-import type { Metadata } from 'next'
-import { Mukta, Montserrat } from 'next/font/google'
-import './globals.css'
-import { Toaster } from '@/components/Toast/Toast'
+import { Footer } from "@/components/Footer/Footer";
+import { Navbar } from "@/components/Navbar/Navbar";
+import type { Metadata } from "next";
+import { Mukta, Montserrat } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/Toast/Toast";
 
 const mukta = Mukta({
-	weight: ['400', '500'],
-	subsets: ['latin'],
-	display: 'swap',
-	variable: '--font-mukta',
-})
+  weight: ["400", "500"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-mukta",
+});
 
 const montserrat = Montserrat({
-	subsets: ['latin'],
-	display: 'swap',
-	variable: '--font-montserrat',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-montserrat",
+});
 
 export const metadata: Metadata = {
-	title: 'CivicDataLab',
-	description:
-		'CivicDataLab is a research and innovation lab that focuses on data, design, and technology for public good.',
-}
+  title: "CivicDataLab",
+  description:
+    "CivicDataLab is a research and innovation lab that focuses on data, design, and technology for public good.",
+  robots: {
+    index: process.env.NODE_ENV === "production",
+    follow: process.env.NODE_ENV === "production",
+  },
+};
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={`${mukta.variable} ${montserrat.variable}`}>
-				<Navbar />
-				{children}
-				<Footer />
-				<Toaster closeButton richColors />
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <body className={`${mukta.variable} ${montserrat.variable}`}>
+        <Navbar />
+        {children}
+        <Footer />
+        <Toaster closeButton richColors />
+      </body>
+    </html>
+  );
 }
