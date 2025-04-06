e651f4ed296922377716974acb1c2c5a2f66ddf7
sanjaypinna <sanjaypinna265@gmail.com>
2025-04-06 07:30:17 +0530

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
-const backendUrl = new URL(process.env.NEXT_PUBLIC_STRAPI_URL || '');
 
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: backendUrl.protocol.replace(':', ''),
-        hostname: backendUrl.hostname,
-        port: backendUrl.port || '', // empty string if no port
-        pathname: '/**',
+        protocol: 'https',
+        hostname: 'dev.strapi.civicdatalab.in',
+        pathname: "/**",
       },
     ],
   },
