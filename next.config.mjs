347b43cf469b3b2aa0f13a3cc061e1a1bc8706f1
sanjaypinna <sanjaypinna265@gmail.com>
2025-04-06 07:34:33 +0530

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'dev.strapi.civicdatalab.in',
-        pathname: "/**",
+        port: '',
+        pathname: '/**',
       },
     ],
   },
